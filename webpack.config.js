90d1b01c5a7b47ae6c90eb518008134aad54965a
David Ramírez <cdavidramirezg24@gmail.com>
2019-07-30 00:50:10 -0500

    Initial commit & configuration

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  output: {
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [new htmlWebpackPlugin({ template: 'src/index.html' })]
+};
